29a6c1026fdafcdf484f259dc0eb2c302450e2b6
Oscar Bazan <oscaribh97@gmail.com>
2026-03-18 16:25:41 -0600

    adding SearchControls component

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,10 +1,18 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
     return (
         <>
             <CustomJumbotron title="Búsqueda de Superheroes"
                 description="Descubre, explora y administra super héroes y villanos" />
+
+            {/* Stats Dashboard */}
+            <HeroStats />
+
+            {/* Filter and Seach */}
+            <SearchControls />
         </>
     )
 };
