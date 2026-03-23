7a570746f7bdff2452842c371acef7c1dd3dfd36
Oscar Bazan <oscaribh97@gmail.com>
2026-03-23 17:18:34 -0600

    changing process file component

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -8,8 +8,11 @@ import { cn } from "@/lib/utils"
 function Progress({
   className,
   value,
+  activeColor = 'bg-primary',
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+}: React.ComponentProps<typeof ProgressPrimitive.Root> & {
+  activeColor?: string
+}) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
@@ -21,7 +24,7 @@ function Progress({
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="size-full flex-1 bg-primary transition-all"
+        className={cn(activeColor, "size-full flex-1 transition-all")}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
