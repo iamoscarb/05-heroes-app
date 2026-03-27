0cc9ba943bb6d07713ca46e55762e407915b5f98
Oscar Bazan <oscaribh97@gmail.com>
2026-03-27 16:43:53 -0600

    adding AdvancedFilters component

--- src/heroes/pages/search/ui/SearchControls.tsx
+++ src/heroes/pages/search/ui/SearchControls.tsx
@@ -2,23 +2,16 @@ import { Search, Filter, SortAsc, Grid, Plus, SortDesc } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Slider } from "@/components/ui/slider"
 import { Accordion, AccordionContent, AccordionItem, } from "@/components/ui/accordion"
-import { CustomSelect } from "@/components/custom/CustomSelect"
-import { teams, categories, universes, statuses } from "../data/selectData"
 import { useSearchControls } from "@/heroes/hooks/useSearchControls"
+import { AdvancedFilters } from "./AdvancedFilters"
 
 
 export const SearchControls = () => {
 
-    const { inputRef, searchParams, setSearchParams, handleKeyDown, handleSelected, handleOrderBy, clearAllFilters, setQueryParams } = useSearchControls();
+    const { inputRef, searchParams, setSearchParams, handleKeyDown, handleOrderBy, setQueryParams } = useSearchControls();
 
     const activeAccordion = searchParams.get('active-accordion') ?? '';
-    const selectedStrength = searchParams.get('strength') ?? '0';
-    const selectedTeam = searchParams.get('team') ?? 'all';
-    const selectedCategory = searchParams.get('category') ?? 'all';
-    const selectedUniverse = searchParams.get('universe') ?? 'all';
-    const selectedStatus = searchParams.get('status') ?? 'all';
     const selectedOrderBy = searchParams.get('order') ?? 'asc'
 
     return (
@@ -73,37 +66,7 @@ export const SearchControls = () => {
             <Accordion type="single" collapsible value={activeAccordion}>
                 <AccordionItem value="advance-filters">
                     <AccordionContent>
-                        <div className="bg-white rounded-lg p-6 mb-8 shadow-sm border">
-                            <div className="flex justify-between items-center mb-4">
-                                <h3 className="text-lg font-semibold">Advanced Filters</h3>
-                                <Button variant="ghost" onClick={clearAllFilters}>Clear All</Button>
-                            </div>
-                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                                <div className="space-y-2">
-                                    <label className="text-sm font-medium">Team</label>
-                                    <CustomSelect key='team' onValueChange={(data) => handleSelected('team', data)} options={teams} selectedValue={selectedTeam} />
-                                </div>
-                                <div className="space-y-2">
-                                    <label className="text-sm font-medium">Category</label>
-                                    <CustomSelect key='category' onValueChange={(data) => handleSelected('category', data)} options={categories} selectedValue={selectedCategory} />
-                                </div>
-                                <div className="space-y-2">
-                                    <label className="text-sm font-medium">Universe</label>
-                                    <CustomSelect key='universe' onValueChange={(data) => handleSelected('universe', data)} options={universes} selectedValue={selectedUniverse} />
-
-                                </div>
-                                <div className="space-y-2">
-                                    <label className="text-sm font-medium">Status</label>
-                                    <CustomSelect key='status' onValueChange={(data) => handleSelected('status', data)} options={statuses} selectedValue={selectedStatus} />
-
-                                </div>
-                            </div>
-                            <div className="mt-4">
-                                <label className="text-sm font-medium">Minimum Strength: {selectedStrength}/10</label>
-                                <Slider defaultValue={[+selectedStrength]} max={10} step={1} value={[+selectedStrength]}
-                                    onValueChange={value => handleSelected('strength', value[0].toString())} />
-                            </div>
-                        </div>
+                        <AdvancedFilters />
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
