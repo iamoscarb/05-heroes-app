c1a02b4ef8449daaecbe189d5c03c84b9a0c8603
Oscar Bazan <oscaribh97@gmail.com>
2026-03-30 16:59:23 -0600

    ::::TESTING:::: installing testing tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
@@ -14,5 +14,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
-  }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
