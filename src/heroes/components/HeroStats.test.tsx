b7191477cc9a1f1b0416dbef48167692ddf7aea3
Oscar Bazan <oscaribh97@gmail.com>
2026-04-01 17:59:06 -0600

    :::: TESTING :::: testing HeroStats

---
+++ src/heroes/components/HeroStats.test.tsx
@@ -0,0 +1,108 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { HeroStats } from "./HeroStats";
+import { useHeroSummary } from "../hooks/useHeroSummary";
+import type { SummaryInformationResponse } from "../types/summary-information.response";
+
+
+vi.mock('../hooks/useHeroSummary');
+const mockUseHeroSummaty = vi.mocked(useHeroSummary);
+
+const mockSumaryData = {
+    "totalHeroes": 25,
+    "strongestHero": {
+        "id": "1",
+        "name": "Clark Kent",
+        "slug": "clark-kent",
+        "alias": "Superman",
+        "powers": [
+            "Súper fuerza",
+            "Vuelo",
+            "Visión de calor",
+            "Visión de rayos X",
+            "Invulnerabilidad",
+            "Súper velocidad"
+        ],
+        "description": "El Último Hijo de Krypton, protector de la Tierra y símbolo de esperanza para toda la humanidad.",
+        "strength": 10,
+        "intelligence": 8,
+        "speed": 9,
+        "durability": 10,
+        "team": "Liga de la Justicia",
+        "image": "1.jpeg",
+        "firstAppearance": "1938",
+        "status": "Active",
+        "category": "Hero",
+        "universe": "DC"
+    },
+    "smartestHero": {
+        "id": "2",
+        "name": "Bruce Wayne",
+        "slug": "bruce-wayne",
+        "alias": "Batman",
+        "powers": [
+            "Artes marciales",
+            "Habilidades de detective",
+            "Tecnología avanzada",
+            "Sigilo",
+            "Genio táctico"
+        ],
+        "description": "El Caballero Oscuro de Ciudad Gótica, que utiliza el miedo como arma contra el crimen y la corrupción.",
+        "strength": 6,
+        "intelligence": 10,
+        "speed": 6,
+        "durability": 7,
+        "team": "Liga de la Justicia",
+        "image": "2.jpeg",
+        "firstAppearance": "1939",
+        "status": "Active",
+        "category": "Hero",
+        "universe": "DC"
+    },
+    "heroCount": 18,
+    "villainCount": 7
+}
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false
+        }
+    }
+})
+
+const renderHeroStats = (mockData?: Partial<SummaryInformationResponse>) => {
+
+    if (mockData) {
+        mockUseHeroSummaty.mockReturnValue({
+            data: mockData
+        } as unknown as ReturnType<typeof useHeroSummary>);
+    } else {
+        mockUseHeroSummaty.mockReturnValue({
+            data: undefined
+        } as unknown as ReturnType<typeof useHeroSummary>);
+    }
+
+    return render(
+        <QueryClientProvider client={queryClient}>
+            <HeroStats />
+        </QueryClientProvider>
+    )
+}
+
+describe('HeroStats', () => {
+    test('should render component with default values', () => {
+        const { container } = renderHeroStats();
+        expect(screen.getByText('Loading...')).toBeDefined();
+        expect(container).toMatchSnapshot();
+    });
+
+    test('should render heroStats with mock information', () => {
+        const { container } = renderHeroStats(mockSumaryData);
+
+        expect(container).toMatchSnapshot();
+        expect(screen.getByText('Total Characters')).toBeDefined();
+        expect(screen.getByText('Favoritos')).toBeDefined();
+    })
+})
